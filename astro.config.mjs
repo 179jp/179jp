5b5e9ee0054f580959366f8dcd8b6f7d14b643da
nnjyami <y@179.jp>
2025-11-13 12:06:55 +0900

    アナリティクスを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
-import mdx from '@astrojs/mdx';
-import svelte from '@astrojs/svelte';
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import svelte from "@astrojs/svelte";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://179.jp',
-  integrations: [tailwind(), mdx(), svelte()]
+  site: "https://179.jp",
+  integrations: [
+    tailwind(),
+    mdx(),
+    svelte(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
